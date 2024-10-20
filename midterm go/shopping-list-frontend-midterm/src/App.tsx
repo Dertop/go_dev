bc734e8447a52cd6cefe1adcd1cda4fdc6d92cae
Dertop <99186380+Dertop@users.noreply.github.com>
2024-10-21 02:24:39 +0500

    midterm

---
+++ midterm go/shopping-list-frontend-midterm/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ItemList from './components/ItemList';
+import ItemForm from './components/ItemForm';
+import './App.css';
+
+const App: React.FC = () => {
+  const [refresh, setRefresh] = useState(false);
+
+  const handleItemCreated = () => {
+    setRefresh(!refresh);
+  };
+
+  return (
+    <div className="App">
+      <ItemForm onItemCreated={handleItemCreated} />
+      <ItemList key={refresh ? 1 : 0} />
+    </div>
+  );
+};
+
+export default App;
